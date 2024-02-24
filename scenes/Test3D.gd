94d78845dd93e8c5b2604062a8c052311f31b27c
Adam Heinermann <aheinerm@gmail.com>
2024-02-23 22:04:48 -0800

    Project setup

---
+++ scenes/Test3D.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@onready var fps_label: Label = $FPSLabel
+
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("exit"):
+		self.get_tree().quit()
+
+
+func _process(delta: float):
+	fps_label.text = "FPS: %f" % Engine.get_frames_per_second()
+
